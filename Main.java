7afd70b2d40e54e523e20ae9aa58f1c2dab33ce4
mihatrebenko <mihatrebenko@mail.ru>
2023-09-20 23:32:47 +0900

    Calculate

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Enter an equation");
         Scanner input = new Scanner(System.in);
